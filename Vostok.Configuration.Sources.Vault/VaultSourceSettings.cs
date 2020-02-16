e3b3cde08db0d87944a92ad49e592eb399f2ccd2
iloktionov <iloktionov@skbkontur.ru>
2020-02-16 17:49:00 +0500

    Xml-docs for VaultSourceSettings.

--- Vostok.Configuration.Sources.Vault/VaultSourceSettings.cs
+++ Vostok.Configuration.Sources.Vault/VaultSourceSettings.cs
@@ -8,6 +8,9 @@ using Vostok.Logging.Abstractions;
 
 namespace Vostok.Configuration.Sources.Vault
 {
+    /// <summary>
+    /// Configuration options of <see cref="VaultSource"/>.
+    /// </summary>
     [PublicAPI]
     public class VaultSourceSettings
     {
@@ -23,24 +26,48 @@ namespace Vostok.Configuration.Sources.Vault
             Log = log ?? LogProvider.Get();
         }
 
+        /// <summary>
+        /// Topology of the Vault cluster.
+        /// </summary>
         [NotNull]
         public IClusterProvider Cluster { get; }
 
+        /// <summary>
+        /// Login method used to obtain a Vault token.
+        /// </summary>
         [NotNull]
         public ILoginMethod Login { get; }
 
+        /// <summary>
+        /// Path to the secret being read (omit the <c>secret/</c> prefix here).
+        /// </summary>
         [NotNull]
         public string Path { get; }
 
+        /// <summary>
+        /// Optional diagnostic log. <see cref="LogProvider"/> is used by default.
+        /// </summary>
         [NotNull]
         public ILog Log { get; }
 
+        /// <summary>
+        /// If enabled (default), polls Vault with <see cref="UpdatePeriod"/> to discover new versions of the secret.
+        /// </summary>
         public bool EnablePeriodicUpdates { get; set; } = true;
 
+        /// <summary>
+        /// Polling period used when <see cref="EnablePeriodicUpdates"/> is <c>true</c>.
+        /// </summary>
         public TimeSpan UpdatePeriod { get; set; } = 1.Minutes();
 
+        /// <summary>
+        /// Default timeout for all Vault requests (auth and secret reads).
+        /// </summary>
         public TimeSpan RequestTimeout { get; set; } = 30.Seconds();
 
+        /// <summary>
+        /// Optional cancellation token that can be used to stop periodic updates (equivalent to disposing the <see cref="VaultSource"/> instance).
+        /// </summary>
         public CancellationToken Cancellation { get; set; } = CancellationToken.None;
     }
 }
